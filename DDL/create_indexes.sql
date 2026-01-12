9e37050a036fb985942815aba442d5e592f5f94e
Vladimir Grishanin <vladimir_grishanin@outlook.com>
2026-01-12 20:50:30 +0300

    Create indexes

--- DDL/create_indexes.sql
+++ DDL/create_indexes.sql
@@ -1,27 +1,45 @@
--- users indexes
-CREATE INDEX idx_users_email_username ON users(email, username);
+-- Users indexes
+CREATE INDEX idx_users_username ON users(username);
 
--- tasks indexes
-CREATE INDEX idx_tasks_task_name ON tasks(task_name);
-CREATE INDEX idx_tasks_assigned_to_status ON tasks(assigned_to, status_id);
+-- Tasks indexes
+-- GIN index for full-text search on task name
+CREATE INDEX idx_tasks_task_name_gin ON tasks USING gin (to_tsvector(task_name));
+
+-- GIN index for full-text search on task description
+CREATE INDEX idx_tasks_task_description_gin ON tasks USING gin (to_tsvector(task_description));
+
+-- Composite partial index to search for tasks assigned to a user in a status == 'completed' (status_id = 3)
+CREATE INDEX idx_tasks_assigned_to_status ON tasks(assigned_to, status_id) WHERE status_id = 3;
+
+-- Composite index to search for tasks of a project and group (order) by status
 CREATE INDEX idx_tasks_project_id_status ON tasks(project_id, status_id);
 
--- change_history indexes
-CREATE INDEX idx_change_history_task_id_changed_by ON change_history(task_id, changed_by);
+-- Log_history indexes
+-- Search log history records of a specific task
+CREATE INDEX idx_log_history_task_id ON log_history(task_id);
 
--- comments indexes
+-- Comments indexes
+-- Search comments by task_id and order by created_at
 CREATE INDEX idx_comments_task_id_created_at ON comments(task_id, created_at);
 
--- files indexes
+-- Files indexes
+-- Search files by task_id
 CREATE INDEX idx_files_task_id ON files(task_id);
 
--- task_dependencies indexes
-CREATE INDEX idx_task_dependencies_dependency_type ON task_dependencies(dependency_type);
+-- Task_dependencies indexes
+-- Search dependent tasks by task_id
+CREATE INDEX idx_task_dependencies_task_id ON task_dependencies(task_id);
 
--- team_members indexes
-CREATE INDEX idx_team_members_user_id_team_id ON team_members(user_id, team_id);
+-- Team_members indexes
+-- Search users in a team
+CREATE INDEX idx_team_members_team_id ON team_members(team_id);
 
--- time_tracking indexes
-CREATE INDEX idx_time_tracking_task_id_user_id ON time_tracking(task_id, user_id);
-CREATE INDEX idx_time_tracking_task_id_entry_date ON time_tracking(task_id, entry_date);
-CREATE INDEX idx_time_tracking_user_id_entry_date ON time_tracking(user_id, entry_date);
+-- Time_tracking indexes
+-- Search time tracking entries by task_id and user_id
+CREATE INDEX idx_time_tracking_task_id_user_id ON time_tracking(task_id, user_id) INCLUDE (hours_spent, entry_date);
+
+-- Search time tracking entries for task by date
+CREATE INDEX idx_time_tracking_task_id_entry_date ON time_tracking(task_id, entry_date) INCLUDE (hours_spent);
+
+-- Search time tracking entries for user by date
+CREATE INDEX idx_time_tracking_user_id_entry_date ON time_tracking(user_id, entry_date) INCLUDE (task_id, hours_spent);
